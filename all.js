dd43568213b5c0b9bf6b649641c8f44e9f9e4955
liltrap999 <103011867+liltrap999@users.noreply.github.com>
2023-08-30 13:09:11 +0100

    w

--- all.js
+++ all.js
@@ -1,7 +1,25 @@
-let head = document.getElementsByTagName('head')[0];
-let link = document.createElement('link');
+const head = document.getElementsByTagName('head')[0];
+
+
+var link, link2, script;
+link = document.createElement('link');
 link.rel = 'stylesheet';
-link.type = 'text/css';
 link.href = '../all.css';
 
+link2 = document.createElement('link');
+link2.rel = 'stylesheet';
+link2.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css';
+
+script = document.createElement('script');
+script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js';
+
+script2 = document.createElement('script');
+script2.src = 'https://code.jquery.com/jquery-3.7.1.min.js'
+script2.integrity = 'sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo='
+script2.crossorigin = 'anonymous';
+
+
+
 head.appendChild(link);
+head.appendChild(link2);
+head.appendChild(script);
--- js/height-converter.js
+++ js/height-converter.js
@@ -3,3 +3,35 @@
 // for (var i = 0; i < units.length; i++) {
 //     units[i].setAttribute('value', units[i].getAttribute('id'));
 // }
+
+let container = document.querySelector('.container');
+const input = document.querySelector('#userInput');        
+const cm = document.querySelector("#centimetres");
+const inches = document.querySelector("#inches");
+const feet = document.querySelector("#feet"); 
+const mtr = document.querySelector("#metres");
+const calc = document.querySelector("#calc");
+const reset = document.querySelector("#reset");
+
+let errorMsg, errorMsg2, errorMsg3;
+
+
+errorMsg = document.createElement('div');
+errorMsg.id = 'error1';
+
+errorMsg2 = document.createElement('div');
+errorMsg2.id = 'error2';
+
+errorMsg3 = document.createElement('div');
+errorMsg3.id = 'error3';
+
+calc.addEventListener('click', () => {
+    if (input.value.trim() === '') {
+        errorMsg.innerHTML = `<p class="alert alert-danger">Input cannot be blank</p> `
+        container.appendChild(errorMsg)
+    }
+
+    if (input.value.trim() === '') {
+        
+    }
+});
