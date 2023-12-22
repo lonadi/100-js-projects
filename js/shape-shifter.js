a39fe9c39a27f8afb15834559bb25da04999b65e
liltrap999 <103011867+liltrap999@users.noreply.github.com>
2023-12-22 18:38:59 +0100

    shape shifter complete

--- js/shape-shifter.js
+++ js/shape-shifter.js
@@ -1,1 +1,19 @@
+// function toggleShape() {
+//     var shapeElement = document.getElementById('shape');
+
+//     // Toggle between different shape classes
+//     if (shapeElement.classList.contains('square')) {
+//         shapeElement.classList.remove('square');
+//         shapeElement.classList.add('circle');
+//     } else if (shapeElement.classList.contains('circle')) {
+//         shapeElement.classList.remove('circle');
+//         shapeElement.classList.add('triangle');
+//     } else {
+//         shapeElement.classList.remove('triangle');
+//         shapeElement.classList.add('square');
+//     }
+// }
+
+// setInterval(toggleShape, 2000);
+
 document.getElementsByClassName('container')[0].insertAdjacentHTML("afterend", footer);
