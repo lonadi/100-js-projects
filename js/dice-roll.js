67f0c9ecd49eccde84ed168e714e5ac6ca18aa40
liltrap999 <103011867+liltrap999@users.noreply.github.com>
2023-12-23 22:04:10 +0100

    pomodoro timer complete + minor c

--- js/dice-roll.js
+++ js/dice-roll.js
@@ -1,55 +1,46 @@
 const buttonEl = document.getElementById("roll-button");
-
 const diceEl = document.getElementById("dice");
-
 const rollHistoryEl = document.getElementById("roll-history");
 
 let historyList = [];
 
-function rollDice() {
-  const rollResult = Math.floor(Math.random() * 6) + 1;
-  const diceFace = getDiceFace(rollResult);
-  diceEl.innerHTML = diceFace;
+const rollDice = () => {
+  // random number between 1 and 6
+  const rollResult = Math.ceil(Math.random() * 6);
+  
+  // Update the dice element with the corresponding Unicode dice face
+  diceEl.innerHTML = `&#9855${rollResult};`;
+  
+  // Add the roll result to the history list and update the history display
   historyList.push(rollResult);
   updateRollHistory();
-}
+};
 
-function updateRollHistory() {
+// Function to update the roll history display
+const updateRollHistory = () => {
+  // Clear the current content of the roll history element
   rollHistoryEl.innerHTML = "";
-  for (let i = 0; i < historyList.length; i++) {
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `Roll ${i + 1}: <span>${getDiceFace(
-      historyList[i]
-    )}</span>`;
-    rollHistoryEl.appendChild(listItem);
-  }
-}
 
-function getDiceFace(rollResult) {
-  switch (rollResult) {
-    case 1:
-      return "&#9856;";
-    case 2:
-      return "&#9857;";
-    case 3:
-      return "&#9858;";
-    case 4:
-      return "&#9859;";
-    case 5:
-      return "&#9860;";
-    case 6:
-      return "&#9861;";
-    default:
-      return "";
-  }
-}
+  // Create HTML for each roll using map and join
+  rollHistoryEl.innerHTML = historyList.map((result, index) =>
+    `<li>Roll ${index + 1}: <span>&#9855${result};</span></li>`
+  ).join('');
+};
 
+// Event listener for the roll button click
 buttonEl.addEventListener("click", () => {
+  // Add a CSS class to initiate the roll animation
   diceEl.classList.add("roll-animation");
+  
+  // Set a timeout to remove the animation class and perform the dice roll after a delay
   setTimeout(() => {
     diceEl.classList.remove("roll-animation");
     rollDice();
-  }, 1000);
+  }, 1000); // Adjust the delay as needed
 });
+
+// Insert HTML content after the first element with the class 'container'
+document.getElementsByClassName('container')[0].insertAdjacentHTML("afterend", footer);
+
 
 document.getElementsByClassName('container')[0].insertAdjacentHTML("afterend", footer);
