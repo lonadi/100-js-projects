4ec6dfaedac89ce6cebabe6ddd5416e6130cc403
liltrap999 <103011867+liltrap999@users.noreply.github.com>
2023-12-28 09:32:28 +0100

    rock paper scissors finalized

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,5 +1,13 @@
+var playerScore = 0;
+var computerScore = 0;
+
+document.getElementById('ps-val').textContent = playerScore;
+document.getElementById('cs-val').textContent = computerScore;
+
 function playGame(playerChoice) {
-    const computerChoice = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+
     const result = (() => {
         if (playerChoice === computerChoice) {
             return "It's a tie!";
@@ -8,11 +16,16 @@ function playGame(playerChoice) {
             (playerChoice === "paper" && computerChoice === "rock") ||
             (playerChoice === "scissors" && computerChoice === "paper")
         ) {
+            playerScore += 1;
+            document.getElementById('ps-val').textContent = playerScore;
             return "You win!";
         } else {
+            computerScore += 1;
+            document.getElementById('cs-val').textContent = computerScore;
             return "Computer wins!";
         }
     })();
     
-    document.getElementById('result').innerText = `You chose ${playerChoice}. Computer chose ${computerChoice}. ${result}`;
+    const resultMessage = `You chose ${playerChoice}. Computer chose ${computerChoice}. ${result}`;
+    document.getElementById('result').textContent = resultMessage;
 }
