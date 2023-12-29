ea8c2e90ef27c7cab126dbaa8726f225588228c3
liltrap999 <103011867+liltrap999@users.noreply.github.com>
2023-12-29 08:24:48 +0100

    minor changes to rock paper scissors

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,11 +1,12 @@
 var playerScore = 0;
 var computerScore = 0;
 var moves = 0;
+
 document.getElementById('ps-val').textContent = playerScore;
 document.getElementById('cs-val').textContent = computerScore;
 document.getElementById('moves-val').textContent = moves;
 
-alert('let\'s see if you can defeat computer in 10 moves')
+alert("Let's see if you can defeat the computer in 20 moves.");
 
 function playGame(playerChoice) {
     const choices = ['rock', 'paper', 'scissors'];
@@ -16,34 +17,35 @@ function playGame(playerChoice) {
             moves += 1;
             document.getElementById('moves-val').textContent = moves;
             return "It's a tie!";
-            
         } else if (
-            (playerChoice === "rock" && computerChoice === "scissors") ||
-            (playerChoice === "paper" && computerChoice === "rock") ||
-            (playerChoice === "scissors" && computerChoice === "paper")
+            (playerChoice === 'rock' && computerChoice === 'scissors') ||
+            (playerChoice === 'paper' && computerChoice === 'rock') ||
+            (playerChoice === 'scissors' && computerChoice === 'paper')
         ) {
             moves += 1;
             playerScore += 1;
             document.getElementById('moves-val').textContent = moves;
             document.getElementById('ps-val').textContent = playerScore;
-            return "You win!";
+            return 'You win!';
         } else {
             moves += 1;
             computerScore += 1;
             document.getElementById('moves-val').textContent = moves;
             document.getElementById('cs-val').textContent = computerScore;
-            return "Computer wins!";
+            return 'Computer wins!';
         }
     })();
-    
-    if (moves === 20 && computerScore > playerScore) {
-        alert("You lost!");
-    } else if (moves === 20 && computerScore < playerScore) {
-        alert("You won!");
-    } else if (moves === 20 && computerScore === playerScore) {
-        alert("You drew!");
-    }
 
     const resultMessage = `${result} You chose ${playerChoice}. Computer chose ${computerChoice}.`;
     document.getElementById('result').textContent = resultMessage;
+
+    if (moves === 20) {
+        if (computerScore > playerScore) {
+            alert('Game over! You lost!');
+        } else if (computerScore < playerScore) {
+            alert('Congratulations! You won!');
+        } else {
+            alert('It\'s a draw!');
+        }
+    }
 }
