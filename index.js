a5f2aa124f8d6990cdfc5274c95f04d6bc17f352
liltrap999 <103011867+liltrap999@users.noreply.github.com>
2024-01-04 16:09:16 +0100

    projects sorted in accordance to the date

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-// var projects = document.querySelectorAll('.project-card');
-
-// projects.forEach(project => project.setAttribute('target', '_blank'));
-
 document.addEventListener('DOMContentLoaded', function () {
     const searchBar = document.getElementById('search');
     const projectList = document.getElementById('project-list');
     const projects = projectList.querySelectorAll('.project-item');
-    const projectTitles = projectList.querySelectorAll('.project-title');
 
     searchBar.addEventListener('input', function () {
         const searchTerm = searchBar.value.toLowerCase();
 
-        // for (let i = 0; i < projectTitles.length; i++) {
-        //     const project = projects[i];
-        //     const projectTitle = projectTitles[i]
-        //     const projectText = projectTitle.innerText.toLowerCase();
-        //     // const projectText = project.innerText.toLowerCase();
-
-        //     if (projectTitle.includes(searchTerm)) {
-        //         project.style.display = 'block';
-        //     } else {
-        //         project.style.display = 'none';
-        //     }
-        // }
-
-    
-
         for (let i = 0; i < projects.length; i++) {
             const project = projects[i];
-            const projectTitle = project.querySelector('.project-title'); // Assuming the title is a child element with class 'project-title'
+            const projectTitle = project.querySelector('.project-title');
             const projectText = projectTitle.innerText.toLowerCase();
 
             if (projectText.includes(searchTerm)) {
@@ -37,6 +17,23 @@ document.addEventListener('DOMContentLoaded', function () {
                 project.style.display = 'none';
             }
         }
-        
+
     });
+
+    const reverseProjects = () => {
+        const projectsList = document.getElementById('project-list');
+
+        const projects = Array.from(projectsList.children);
+
+        projects.reverse();
+
+        projectsList.innerHTML = '';
+
+        projects.forEach(item => {
+            projectsList.appendChild(item);
+        });
+    }
+
+    reverseProjects();
+
 });
