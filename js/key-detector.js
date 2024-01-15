6540c850a952b4fbcce56e2e99902d544bdb1022
liltrap999 <103011867+liltrap999@users.noreply.github.com>
2024-01-15 21:48:57 +0100

    testing

--- js/key-detector.js
+++ js/key-detector.js
@@ -1,21 +1,30 @@
 const insert = document.getElementById('insert');
+const textInput = document.getElementById('textInput');
 
-document.querySelector('input').click()
+function handleKeyEvent(e) {
+    console.log(e);
+    insert.innerHTML = `
+        <div class="key">
+            ${e.key === ' ' ? 'Space' : e.key}
+            <small>Key</small>
+        </div>
+        <div class="key">
+            ${e.keyCode}
+            <small>Key Code</small>
+        </div>
+        <div class="key">
+            ${e.code}
+            <small>Code</small>
+        </div>
+    `;
+}
 
-window.addEventListener('keydown', (e) => {
-	console.log(e);
-	insert.innerHTML = `
-		<div class="key">
-			${e.key === ' ' ? 'Space' : e.key}
-			<small>Key</small>
-		</div>
-		<div class="key">
-			${e.keyCode}
-			<small>Key Code</small>
-		</div>
-		<div class="key">
-			${e.code}
-			<small>Code</small>
-		</div>
-	`
+window.addEventListener('keydown', handleKeyEvent);
+textInput.addEventListener('input', (e) => {
+    const virtualKeyEvent = {
+        key: e.data || 'Enter',
+        keyCode: e.inputType === 'insertText' ? e.data.charCodeAt(0) : 13,
+        code: e.inputType === 'insertText' ? 'Enter' : '',
+    };
+    handleKeyEvent(virtualKeyEvent);
 });
