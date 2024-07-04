b5eddd8f68a51613b01c454e99351ae375f6768b
liltrap999 <103011867+liltrap999@users.noreply.github.com>
2024-07-04 22:46:13 +0100

    minor

--- js/sentiment.js
+++ js/sentiment.js
@@ -8,7 +8,7 @@ document.querySelector('#sentimentForm').addEventListener('submit', (event)=> {
         headers: { 'X-Api-Key': 'Ft1IHWqsLRAXiTXv4EjSOA==1loxHFAeM65TkNeR' },
         contentType: 'application/json',
         success: function (result) {
-            $('#resultContainer').html('<p>Sentiment: ' + result.sentiment + '</p>' + '<br/>' + '<p>Score:' + result.score + '</p>');
+            $('#resultContainer').html('<p>Sentiment: ' + result.sentiment + '</p>' + '<br/>' + '<p>Score: ' + result.score + '</p>');
         },
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
